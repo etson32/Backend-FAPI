bb5bdf6e025fcf182a630939ccd34ef338a02e96
Etson <etson32@gmail.com>
2024-11-05 11:56:40 -0500

    deploy api

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -395,6 +395,7 @@ async def get_propuesta_by_id(id_tesis: int, db: Session = Depends(get_db)):
 
 
 # Lanzar configuracion automatica para ejecucion
+# Para pruebas locales
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True) 
+#if __name__ == '__main__':
+#    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True) 
